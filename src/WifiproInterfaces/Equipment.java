c52a88bc3801235bc1e84a6c256f2e25f0e0c55b
hi βεckї <hi ck>
2016-07-27 14:46:22 +0700

    test

--- src/WifiproInterfaces/Equipment.java
+++ src/WifiproInterfaces/Equipment.java
@@ -15,7 +15,7 @@ public class Equipment {
 	public long id;
 	public String host_name; 
 	public String ip;
-	public String location;
+	public String location_id;
 	public String latitude;
 	public String longitude;
 	public String status;
@@ -47,9 +47,24 @@ public class Equipment {
 			cntn = DriverManager.getConnection(Utils.JdbcConnectionstring);
 			stmt = cntn.createStatement();
 
-			String queryString = "select se.*";
-			queryString += " from site_equipment se";
-			queryString += " where se.id = " + equipmentid;
+			//String queryString = "select se.*";
+			//queryString += " from site_equipment se";
+			//queryString += " where se.id = " + equipmentid;
+			
+			String queryString = "select se.*, sae.item_id, sae.service_area_id, i.mac_address, i.serial_no, sc.site_id";
+			queryString += " from item i, site_circuit sc, service_area_equipment sae, site_equipment se";
+			queryString += " where se.service_area_equipment_id = sae.id and se.site_circuit_id = sc.id and sae.item_id = i.id";
+			queryString += " and se.id = " + equipmentid;
+		
+			//select se.*, sae.item_id, sae.service_area_id, i.mac_address, i.serial_no, sc.site_id from item i, site_circuit sc, service_area_equipment sae, site_equipment se where se.service_area_equipment_id = sae.id and se.site_circuit_id = sc.id and sae.item_id = i.id and se.id = 1192;
+			
+			// service_area_equipment_id = 427 in service_area_equipment get service_area = 413
+			//select * from site_equipment where id = 1192;
+			// select * from service_area_equipment where id = 426;
+			
+			// select * from site where service_area_id = 413;
+			// How site table find equipment???
+			
 			
 System.out.println("queryString "+queryString);
 			rsst = stmt.executeQuery(queryString);
@@ -59,7 +74,7 @@ System.out.println("queryString "+queryString);
 				this.ip = rsst.getString("ip");
 				this.latitude = rsst.getString("latitude");
 				this.longitude = rsst.getString("longitude");
-				this.location = rsst.getString("location");
+				this.location_id = rsst.getString("site_id");
 				this.status = rsst.getString("status");
 				this.equipment_id = rsst.getString("equipment_id");
 				this.service_area_equipment_id = rsst.getLong("service_area_equipment_id");
@@ -109,7 +124,7 @@ LOG.log(Level.INFO,"{0}-errorCode: {1},errorMessage: {2},EquipmentId: {3}",new O
 			obj.put("ip",this.ip);
 			obj.put("latitude",this.latitude);
 			obj.put("longitude",this.longitude);
-			obj.put("location",this.location);
+			obj.put("location_id",this.location_id);
 			obj.put("equipment_id",this.equipment_id);
 			obj.put("status",this.status);
 			//obj.put("service_area_equipment_id",new Long(this.service_area_equipment_id));
